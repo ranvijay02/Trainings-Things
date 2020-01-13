446c01347d4241106325eaa46a93565214bd7c55
RANVIJAY TANWAR <42860273+ranvijay02@users.noreply.github.com>
2020-01-13 09:28:07 +0530

    Add files via upload
    Java

---
+++ limit-service/src/test/java/com/deloitte/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
